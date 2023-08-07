9ffc391ab76bcd698bda7066db25363f4375e784
Pritam Paul <pritampaulsdev@gmail.com>
2023-08-07 14:43:32 +0530

    Initial setting page design started

--- app/src/main/java/com/example/aichatbot/setting_activity.java
+++ app/src/main/java/com/example/aichatbot/setting_activity.java
@@ -2,19 +2,13 @@ package com.example.aichatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class setting_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
+        setContentView(R.layout.activity_setting);
     }
 }
